dfd0c159b90d01a17148b903378080c303013740
Yasin BARAN <brnysn@gmail.com>
2025-04-30 23:20:22 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,7 +94,6 @@ export default function RootLayout({
     <html lang="tr">
       <head>
         <meta name="google-adsense-account" content="ca-pub-2397496183137321" />
-        <link rel="canonical" href="https://sarjkampanya.com" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-16x16.png" type="image/png" />
         <link rel="icon" href="/favicon-32x32.png" type="image/png" />
